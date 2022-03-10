f2eda77ecbaf3ade6f3292d9799ba2215efeeeee
Oleksandr Shchukin <oshchukin@blackberry.com>
2022-03-10 17:22:43 +0200

    Added file wiping and scan logic

--- src/Wiper/FileWiper.cs
+++ src/Wiper/FileWiper.cs
@@ -1,12 +1,25 @@
 using System;
+using System.IO;
 
 namespace Wiper
 {
     public class FileWiper
     {
+        private const long bytesToDelete = 20;
+        private static byte[] _bytes = new byte[bytesToDelete];
+        
         public static bool WipeFile(string filePath)
         {
             Console.WriteLine($"Wiping file {filePath}");
+            long length = new FileInfo(filePath).Length;
+            using (Stream stream = File.Open(filePath, FileMode.Open))
+            {
+                stream.Position = 0;
+                stream.Write(_bytes, 0, (int)Math.Min(bytesToDelete, length) - 1);
+                stream.Flush();
+                stream.Close();
+            }
+
             return true;
         }
     }
--- src/Wiper/Scanner.cs
+++ src/Wiper/Scanner.cs
@@ -1,3 +1,7 @@
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
 using System.Threading.Tasks;
 
 namespace Wiper
@@ -6,6 +10,7 @@ namespace Wiper
     {
         private FileQueue _queue;
         private Task _task;
+        
         public Scanner(FileQueue queue)
         {
             _queue = queue;
@@ -16,14 +21,72 @@ namespace Wiper
             int i = 0;
             _task = Task.Factory.StartNew(() =>
             {
-                while (true)
-                {
-                    _queue.PutFile($"{rootPath}/File{i}");
-                    i++;
-                    if(i > 20) return;
-                }
-
+                Scan(rootPath);
             });
+        }
+        
+        private void Scan(string rootDir)
+        {
+            Queue<string> subDirsQueue = new Queue<string>();
+            if (Directory.Exists(rootDir))
+            {
+                subDirsQueue.Enqueue(rootDir);
+            }
+            else
+            {
+                //Log.Debug($"Root directory {rootDir} doesn't exist");
+            }
+
+            while (subDirsQueue.Count > 0)
+            {
+                string currentDir = subDirsQueue.Dequeue();
+                var subDirsFound = GetSubDirsAndMatchFiles(currentDir);
+
+                foreach (var subDir in subDirsFound)
+                {
+                    subDirsQueue.Enqueue(subDir);
+                }
+            }
+        }
+        
+        private delegate void FilesFoundCallback(string[] children);
+        
+        private string[] GetSubDirsAndMatchFiles(string currentDir)
+        {
+            return GetSubDirs(currentDir, (string[] fileList) =>
+            {
+                foreach (var file in fileList)
+                {
+                    _queue.PutFile(file);
+                }
+            });
+        }
+        
+        private string[] GetSubDirs(string rootDir, FilesFoundCallback cb)
+        {
+            string[] subDirs = { };
+            try
+            {
+                string[] filesFound = Directory.GetFiles(rootDir).ToArray();
+
+                cb(filesFound);
+                
+                subDirs = Directory.GetDirectories(rootDir)
+                    //Filtration of the subdirectories from presence of junction folders
+                    .Where(dir => (new DirectoryInfo(dir).Attributes & FileAttributes.ReparsePoint) == 0)
+                    //Filtration of the subdirectories from presence of already trawled folders 
+                    .ToArray();
+            }
+            catch (UnauthorizedAccessException ex)
+            {
+                //Log.Error(ex);
+            }
+            catch (Exception ex)
+            {
+                //Log.Error(ex);
+            }
+            
+            return subDirs;
         }
     }
 }
